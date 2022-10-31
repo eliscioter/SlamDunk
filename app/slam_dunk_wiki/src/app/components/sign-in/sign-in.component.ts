4cfe06ef4030b5f38ba82148623e27be0c5e7406
eliscioter~ <attila433lehun@gmail.com>
2022-10-31 17:06:54 +0800

    sign in functionality implemented

--- app/slam_dunk_wiki/src/app/components/sign-in/sign-in.component.ts
+++ app/slam_dunk_wiki/src/app/components/sign-in/sign-in.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog'
 
+import { Member } from '../../interfaces/Member'
+import { MemberService } from '../../services/member/member.service';
+
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
@@ -8,12 +11,30 @@ import { MatDialogRef } from '@angular/material/dialog'
 })
 export class SignInComponent implements OnInit {
 
-  constructor(private dialog: MatDialogRef<SignInComponent>) { }
+  username!: string
+  password!: string
+
+  constructor(private dialog: MatDialogRef<SignInComponent>, private member: MemberService) { }
 
   ngOnInit(): void {
   }
 
   closeDialog() {
     this.dialog.close();
+  }
+
+  onSignIn() {
+    const member: Member = {
+      username: this.username,
+      password: this.password
+    }
+    this.member.auth(member).subscribe(
+      next => {
+        alert('Sign in successful')
+      },
+      error => {
+        alert('Sign in failed')
+      } 
+    )
   }
 }
