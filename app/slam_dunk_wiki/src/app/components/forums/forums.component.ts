ec01e8b1559adc3bbdb5cad22518180522950cfc
eliscioter~ <attila433lehun@gmail.com>
2022-11-03 00:18:39 +0800

    made component dynamic

--- app/slam_dunk_wiki/src/app/components/forums/forums.component.ts
+++ app/slam_dunk_wiki/src/app/components/forums/forums.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
+import { MemberService } from '../../services/member/member.service';
+import { ForumService } from '../../services/forum/forum.service';
+import { Forum } from '../../interfaces/Forum';
 @Component({
   selector: 'app-forums',
   templateUrl: './forums.component.html',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForumsComponent implements OnInit {
 
-  constructor() { }
+  forums: Forum[] = []
+  forumCreated: string[] = []
+  forum_id!: string
+
+  constructor(private forum: ForumService, private member: MemberService, private router: Router) { }
 
   ngOnInit(): void {
+    this.forum.getForums().subscribe( (data) => {
+        this.forums = data
+        for (let item = 0; item < data.length; item++) {
+          this.forum_id = (data[item]._id)
+          this.forumCreated.push(new Date(data[item].createdAt).toUTCString())
+        }
+    } );
+  }
+
+  verifyAuth(id: string) {
+    if(!this.member.isLoggedIn()) return alert('You need to sign in first to see the forum thread')
+    else return this.router.navigate(['/forum/thread',id])
   }
 
 }
