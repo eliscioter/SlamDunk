2da445821253b6dece8c557e2b0ae50986825433
eliscioter~ <attila433lehun@gmail.com>
2022-11-06 19:44:48 +0800

    fixed jwt issue

--- server/Forum/app/middlewares/VerifyRole.js
+++ server/Forum/app/middlewares/VerifyRole.js
@@ -3,12 +3,11 @@ export const verifyRole = (...allowedRoles) => {
         try {
             if(!req?.roles) return res.status(401).json({message: 'Invalid user'})
             const rolesArray = [...allowedRoles] 
-            console.log(rolesArray)
             const result = req.roles.map(role => rolesArray.includes(role)).find(val => val === true)
             if(!result) return res.status(401).json({message: 'User do not have permission'})
             next()
         } catch (error) {
-            res.status(401).json({message: error.message})
+            return res.status(401).json({message: error.message})
         }
     }
 }
