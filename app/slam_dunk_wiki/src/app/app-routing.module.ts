8c88a95725ba237b8ca35428a6a8e03219da9954
eliscioter~ <attila433lehun@gmail.com>
2022-11-08 00:14:40 +0800

    added guard, admin login, and route title

--- app/slam_dunk_wiki/src/app/app-routing.module.ts
+++ app/slam_dunk_wiki/src/app/app-routing.module.ts
@@ -1,24 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard } from './guard/auth.guard';
 import { HomeComponent } from './pages/home/home.component';
 import { ForumComponent } from './pages/forum/forum.component';
 import { ForumThreadComponent } from './pages/forum-thread/forum-thread.component';
 import { PlayersComponent } from './pages/players/players.component';
 import { AboutComponent } from './pages/about/about.component';
 import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './admin/pages/login/login.component';
+import { Title } from '@angular/platform-browser';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent},
-  { path: 'forum', component: ForumComponent},
-  { path: 'forum/thread/:id', component: ForumThreadComponent},
-  { path: 'players', component: PlayersComponent},
-  { path: 'about', component: AboutComponent},
-  { path: 'register', component: RegisterComponent},
+  { path: '', component: HomeComponent, data: {title: 'Slam Dunk Wiki'}},
+  { path: 'forum', component: ForumComponent, data: {title: 'Forums'}},
+  { path: 'forum/thread/:id', component: ForumThreadComponent, data: {title: 'Thread'},canActivate: [AuthGuard]},
+  { path: 'players', component: PlayersComponent, data: {title: 'Players'}},
+  { path: 'about', component: AboutComponent, data: {title: 'About'}},
+  { path: 'register', component: RegisterComponent, data: {title: 'Register'}},
+  { path: 'admin/login', component: LoginComponent, data: {title: 'Admin | Login'}},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes,{ onSameUrlNavigation: 'reload' })],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [Title]
 })
 export class AppRoutingModule { }
