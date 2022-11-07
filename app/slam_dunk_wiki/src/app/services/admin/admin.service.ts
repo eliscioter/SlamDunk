45a5bca8d52a25fa4713fc9a8c0c690e5343b59f
eliscioter~ <attila433lehun@gmail.com>
2022-11-08 00:18:59 +0800

    added admin service

---
+++ app/slam_dunk_wiki/src/app/services/admin/admin.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+
+  constructor() { }
+
+  isAdmin(flag: boolean): boolean {
+    return flag
+  }
+
+  
+}
