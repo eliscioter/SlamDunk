4150b0530b3fb8c737c415a14886109dd7bc1824
eliscioter~ <attila433lehun@gmail.com>
2022-11-08 23:14:08 +0800

    switched to new service

---
+++ app/slam_dunk_wiki/src/app/services/user/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { }
+
+  isLoggedIn(): boolean {
+    const authToken = !!localStorage.getItem('access_token');
+    return authToken ? true : false;
+  }
+
+  getUsername(): string {
+    return null || JSON.parse(localStorage.getItem('username') as string)
+  }
+
+  getToken(): string {
+    return localStorage.getItem('access_token') as string
+  }
+
+  getRefreshToken(): string {
+    return localStorage.getItem('refresh_token') as string
+  }
+
+  getRole(): string {
+    return localStorage.getItem('role') as string
+  }
+}
