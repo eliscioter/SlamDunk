d0f27414a26c19cef9eda6010f661dda708dee24
eliscioter~ <attila433lehun@gmail.com>
2022-11-08 23:09:48 +0800

    fixed sign in issue

--- app/slam_dunk_wiki/src/app/admin/pages/login/login.component.ts
+++ app/slam_dunk_wiki/src/app/admin/pages/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { AdminService } from 'src/app/services/admin/admin.service';
+import { UserService } from 'src/app/services/user/user.service';
+import { Admin } from 'src/app/interfaces/admin/Admin';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,10 +11,24 @@ import { AdminService } from 'src/app/services/admin/admin.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private admin: AdminService) { }
+  username!: string
+  password!: string
+  constructor(private admin: AdminService, private user: UserService, private router: Router) { }
 
-  ngOnInit(): void {
-    this.admin.isAdmin(true)
+  ngOnInit(): void {}
+  onLogin(): void {
+    const user: Admin = {
+      username: this.username,
+      password: this.password
+    }
+    this.admin.auth(user).subscribe({
+      next: () => {
+        this.router.navigate(['/create'], { replaceUrl: true })
+        
+        alert('Welcome admin!')
+      },
+      error: () => {alert('Login failed')}
+    })
   }
 
 }
