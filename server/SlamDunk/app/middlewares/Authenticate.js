f8b7450dec33b47633026adce44b4bf5a3e404a3
eliscioter~ <attila433lehun@gmail.com>
2022-11-11 22:31:51 +0800

    updated midllewares

--- server/SlamDunk/app/middlewares/Authenticate.js
+++ server/SlamDunk/app/middlewares/Authenticate.js
@@ -7,9 +7,10 @@ const { ACCESS_TOKEN, REFRESH_TOKEN } = process.env
 
 export async function authenticateToken(req, res, next) {
     try {
-        const header_token = await req.headers['authorization']
-        if (!header_token) return res.status(401).json({ message: 'Token undefined'})
+        const header_token = await req.header('authorization')
+        if (!header_token && header_token === 'null') return res.status(401).json({ message: 'Token undefined'})
         const token = header_token.split(' ')[1]
+        if(token === 'null') return res.status(401).json({ message: 'Token null'})
         jwt.verify(token, ACCESS_TOKEN, (err, decoded) => {
             if(err) return res.status(403).json({ message: err.message })
             req.id = decoded.user_info.id
@@ -18,6 +19,6 @@ export async function authenticateToken(req, res, next) {
         })
         next()
     } catch (error) {
-        res.status(403).json({ message: error.message })
+        return res.status(403).json({ message: error.message })
     }
 }
--- server/SlamDunk/app/middlewares/Error.js
+++ server/SlamDunk/app/middlewares/Error.js
@@ -4,11 +4,17 @@ dotenv.config()
 const { NODE_ENV } = process.env
 
 export const errorHandler = (err, req, res, next) => {
-    if(err) {
-        res.status(res.statusCode).json({ 
-            message: err.message,
-            stack: NODE_ENV === 'DEVELOPEMT' ? err.stack : undefined
-        })
+    try {
+        next()
+    } catch (error) {
+        if(err) {
+            res.status(res.statusCode).json(
+                {
+                    message: err.message,
+                    stack: NODE_ENV === 'DEVELOPEMT' ? err.stack : undefined
+                }
+            )
+            return
+        }
     }
-    next()
 }
