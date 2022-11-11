d6f9dbff2f1f7d3301678f190c2178ce4d3d31a6
Johnrod <johnrodjardeleza09@gmail.com>
2022-11-11 21:48:37 +0800

    Added Traits for Offense, Defense, and Trick

---
+++ app/slam_dunk_wiki/src/app/pages/traits/defense/defense.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-defense',
+  templateUrl: './defense.component.html',
+  styleUrls: ['./defense.component.css']
+})
+export class DefenseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
