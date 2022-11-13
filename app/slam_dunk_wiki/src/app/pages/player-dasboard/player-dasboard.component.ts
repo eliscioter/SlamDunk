67c04e06703fbe06a35426ceaf9d478df48df175
eliscioter~ <attila433lehun@gmail.com>
2022-11-13 11:56:39 +0800

    changed player dashboard layout

---
+++ app/slam_dunk_wiki/src/app/pages/player-dasboard/player-dasboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-player-dasboard',
+  templateUrl: './player-dasboard.component.html',
+  styleUrls: ['./player-dasboard.component.css']
+})
+export class PlayerDasboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
