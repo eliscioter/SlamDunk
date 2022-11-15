05fd3f213780708c9d9cb2f5cbb7fdb2699e3207
eliscioter~ <attila433lehun@gmail.com>
2022-11-15 16:13:42 +0800

    made thank you page personal

--- app/slam_dunk_wiki/src/app/pages/thanks/thanks.component.ts
+++ app/slam_dunk_wiki/src/app/pages/thanks/thanks.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserService } from 'src/app/services/user/user.service';
 @Component({
   selector: 'app-thanks',
   templateUrl: './thanks.component.html',
   styleUrls: ['./thanks.component.css']
 })
 export class ThanksComponent implements OnInit {
-
-  constructor() { }
+  name!: string
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.name = this.userService.getUsername()
   }
 
 }
