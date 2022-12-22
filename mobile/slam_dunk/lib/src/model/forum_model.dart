8fba9a66b9e7aeb95cca015c629d3369910c3cdb
eliscioter~ <attila433lehun@gmail.com>
2022-12-22 21:31:59 +0800

    added forum model

---
+++ mobile/slam_dunk/lib/src/model/forum_model.dart
@@ -0,0 +1,41 @@
+// To parse this JSON data, do
+//
+//     final forums = forumsFromJson(jsonString);
+
+import 'dart:convert';
+
+List<Forums> forumsFromJson(String str) => List<Forums>.from(json.decode(str).map((x) => Forums.fromJson(x)));
+
+String forumsToJson(List<Forums> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class Forums {
+    Forums({
+        this.id,
+        this.title,
+        this.primaryAuthor,
+        this.createdAt,
+        this.updatedAt,
+    });
+
+    String? id;
+    String? title;
+    String? primaryAuthor;
+    DateTime? createdAt;
+    DateTime? updatedAt;
+
+    factory Forums.fromJson(Map<String, dynamic> json) => Forums(
+        id: json["_id"],
+        title: json["title"],
+        primaryAuthor: json["primary_author"],
+        createdAt: DateTime.parse(json["createdAt"]),
+        updatedAt: DateTime.parse(json["updatedAt"]),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "_id": id,
+        "title": title,
+        "primary_author": primaryAuthor,
+        "createdAt": createdAt?.toIso8601String(),
+        "updatedAt": updatedAt?.toIso8601String(),
+    };
+}
