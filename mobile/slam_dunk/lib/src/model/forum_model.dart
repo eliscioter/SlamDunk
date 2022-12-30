3e6c71d0bd895c9478acf827fc4b571ddd35cb08
eliscioter~ <attila433lehun@gmail.com>
2022-12-30 13:12:02 +0800

    removed comments

--- mobile/slam_dunk/lib/src/model/forum_model.dart
+++ mobile/slam_dunk/lib/src/model/forum_model.dart
@@ -1,41 +1,36 @@
-// To parse this JSON data, do
-//
-//     final forums = forumsFromJson(jsonString);
-
 import 'dart:convert';
 
-List<Forums> forumsFromJson(String str) => List<Forums>.from(json.decode(str).map((x) => Forums.fromJson(x)));
+List<Forums> forumsFromJson(String str) => List<Forums>.from(json.decode(str.toString()).map((x) => Forums.fromJson(x)));
 
 String forumsToJson(List<Forums> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class Forums {
-    Forums({
-        this.id,
-        this.title,
-        this.primaryAuthor,
-        this.createdAt,
-        this.updatedAt,
-    });
+  Forums({
+    this.id,
+    this.title,
+    this.primaryAuthor,
+    this.createdAt,
+    this.updatedAt,
+  });
 
-    String? id;
-    String? title;
-    String? primaryAuthor;
-    DateTime? createdAt;
-    DateTime? updatedAt;
+  String? id;
+  String? title;
+  String? primaryAuthor;
+  DateTime? createdAt;
+  DateTime? updatedAt;
 
-    factory Forums.fromJson(Map<String, dynamic> json) => Forums(
-        id: json["_id"],
-        title: json["title"],
-        primaryAuthor: json["primary_author"],
-        createdAt: DateTime.parse(json["createdAt"]),
-        updatedAt: DateTime.parse(json["updatedAt"]),
-    );
+  factory Forums.fromJson(Map<String, dynamic> json) => Forums(
+      id: json["_id"],
+      title: json["title"],
+      primaryAuthor: json["primary_author"],
+      createdAt: DateTime.parse(json["createdAt"]),
+      updatedAt: DateTime.parse(json["updatedAt"]));
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "_id": id,
         "title": title,
         "primary_author": primaryAuthor,
         "createdAt": createdAt?.toIso8601String(),
         "updatedAt": updatedAt?.toIso8601String(),
-    };
+      };
 }
