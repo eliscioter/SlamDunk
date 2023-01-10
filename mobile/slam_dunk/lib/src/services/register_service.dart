de22c091179426edc4fbf5e1066cc311c4d84dfa
eliscioter~ <attila433lehun@gmail.com>
2023-01-10 22:36:54 +0800

    register service

---
+++ mobile/slam_dunk/lib/src/services/register_service.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+
+class RegisterService {
+  final Dio _client = Dio();
+  static const String _baseUrl = 'https://slamdunkusers.onrender.com/api/user';
+  // static const String _baseUrl = 'http://localhost:5002/api/user';
+
+  onSignUp(Map<String, String> userInfo) async {
+    try {
+      final Response response =
+          await _client.post('$_baseUrl/store', data: userInfo);
+      if (response.statusCode == 201) {
+        return true;
+      }
+      return false;
+    } catch (e) {
+      throw Exception('Failed to register: $e');
+    }
+  }
+}
