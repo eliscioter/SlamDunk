8e574ba41f642784b70a63319ebb885c6ab7af3e
eliscioter~ <attila433lehun@gmail.com>
2023-01-31 23:59:46 +0800

    implemented real-time app for forum

--- server/Forum/index.js
+++ server/Forum/index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import { Server } from 'socket.io'
+import { createServer } from "http";
 
 import { router as forumRouter } from './app/router/ForumRouter.js'
 import { errorHandler } from './app/middlewares/Error.js'
@@ -11,6 +13,13 @@ dotenv.config()
 const { PORT, CLIENT_ORIGIN, DB_URI } = process.env
 
 const app = express()
+const server = createServer(app);
+const io = new Server(server, { 
+    cors: {
+        origin: CLIENT_ORIGIN,
+        credentials: true
+    },
+})
 
 app.use(express.json())
 app.use(express.urlencoded({
@@ -24,5 +33,29 @@ app.use(cors({
 app.use('/api/data', forumRouter)
 app.use(errorHandler)
 
-app.listen(PORT, () => console.log(`Listening to port ${PORT}...`))
+server.listen(PORT, () => console.log(`Listening to port ${PORT}...`))
 mongoose.connect(DB_URI, () => console.log(`Connected to the database...`))
+
+io.on("connection", (socket) => {
+    socket.on('forum', (msg, withDeletedForum) => {
+        if(withDeletedForum) {
+            socket.broadcast.emit('receive-new-forums', msg)
+            return
+        }
+        socket.broadcast.emit('receive-forum', msg)
+    })
+    socket.on('join-thread', thread => {
+        socket.join(thread)
+    })
+    socket.on('thread', (thread, msg, withDeletedComment) => {
+        console.log(withDeletedComment)
+        if(withDeletedComment) {
+            socket.to(thread).emit('new-comment', msg)
+            return
+        }
+        socket.to(thread).emit('comment', msg)
+    })
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    })
+})
