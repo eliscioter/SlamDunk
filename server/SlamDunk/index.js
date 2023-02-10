f570172b9ba4a687272e4024f1e242c1cc00f898
eliscioter~ <attila433lehun@gmail.com>
2023-02-10 22:24:20 +0800

    implemented socket io in server

--- server/SlamDunk/index.js
+++ server/SlamDunk/index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
+import { Server } from 'socket.io'
+import { createServer } from "http";
 
 dotenv.config()
 
@@ -12,7 +14,13 @@ import { router as teamRouter } from './app/router/TeamRouter.js'
 const { PORT, CLIENT_ORIGIN, DB_URI } = process.env
 
 const app = express()
-
+const server = createServer(app);
+const io = new Server(server, { 
+    cors: {
+        origin: CLIENT_ORIGIN,
+        credentials: true
+    },
+})
 app.use(express.json())
 app.use(express.urlencoded({ 
     extended: true
@@ -25,5 +33,34 @@ app.use(cors({
 app.use("/api/data", teamRouter)
 app.use(errorHandler)
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}...`))
+server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}...`))
 mongoose.connect(DB_URI, () => console.log(`Connected to the database...`))
+
+io.on("connection", (socket) => {
+    socket.on('player', (players, id, withDeletedPlayer, withUpdatedPlayer) => {
+        if(withDeletedPlayer) {
+            socket.broadcast.emit('receive-new-players', players)
+            return
+        }
+        if(withUpdatedPlayer) {
+            socket.broadcast.emit('receive-updated-players', players)
+            return
+        }
+        socket.broadcast.emit('receive-players', players, id)
+    })
+    socket.on('trait', (traits, id, withDeletedTrait, withUpdatedPlayer) => {
+        if(withDeletedTrait) {
+            socket.broadcast.emit('receive-new-traits', traits)
+            return
+        }
+        if(withUpdatedPlayer) {
+            socket.broadcast.emit('receive-updated-traits', traits)
+            return
+        }
+        socket.broadcast.emit('receive-traits', traits, id)
+    })
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    })
+})
