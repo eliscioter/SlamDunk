4603d701d52daf450fdc68edf4ec8a62cc6f6c13
eliscioter~ <attila433lehun@gmail.com>
2023-02-10 22:23:38 +0800

    made admin traits real-time

--- app/slam_dunk_wiki/src/app/admin/pages/traits-admin/traits-admin.component.ts
+++ app/slam_dunk_wiki/src/app/admin/pages/traits-admin/traits-admin.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { faTrash, faEdit } from '@fortawesome/free-solid-svg-icons';
 import { ToastrService } from 'ngx-toastr';
+import * as io from 'socket.io-client'
 
 import { Trait } from 'src/app/interfaces/Traits';
 import { TraitsService } from 'src/app/services/traits/traits.service';
+
+declare var $: any;
+
 @Component({
   selector: 'app-traits-admin',
   templateUrl: './traits-admin.component.html',
@@ -20,16 +24,38 @@ export class TraitsAdminComponent implements OnInit {
   imgURL!: string
   tag!: string
   buttonName!: string
+  private socket: any
 
   constructor(private traitsService: TraitsService, private toast: ToastrService) { }
 
   ngOnInit(): void {
+    this.displayTraits()
+  }
+
+  displayTraits() {
+    // this.socket = io.io(`http://localhost:5000`)
+    this.socket = io.io(`https://slamdunk.onrender.com`)
     this.traitsService.getTraits().subscribe({
-      next: trait => {
-        this.traits = trait
+      next: traitData => {
+        this.socket.on('receive-traits', (trait: Trait, id: string) => {
+          this.traits.push({...trait, _id: id})
+        })
+        this.socket.on('receive-new-traits', (trait: Trait[]) => {
+          this.traits.splice(0, this.traits.length, ...trait)
+        })
+        this.socket.on('receive-updated-traits', (trait: Trait) => {
+          const updatedTraits = this.traits.map(item => {
+            if (item._id === trait._id) {
+              return trait
+            }
+            return item
+          })
+          this.traits = updatedTraits
+          return
+        })
+        this.traits = traitData
       }
     })
-    
   }
 
   createButton(): string {
@@ -45,6 +71,8 @@ export class TraitsAdminComponent implements OnInit {
   }
 
   create_trait() {
+    // this.socket = io.io(`http://localhost:5000`)
+    this.socket = io.io(`https://slamdunk.onrender.com`)
     const trait: Trait = {
       name: this.title,
       description: this.description,
@@ -52,9 +80,12 @@ export class TraitsAdminComponent implements OnInit {
       tag: this.tag
     }
     this.traitsService.createTrait(trait).subscribe({
-      next: () => {
-        this.toast.success(`Create ${this.title} successfully`)
-        window.location.reload()
+      next: traitData => {       
+
+        this.socket.emit('trait', trait, traitData._id, false, false)
+        $('#create_trait').modal('hide')
+
+        this.toast.success(`Created ${this.title} successfully`)
       }, error: () => this.toast.error('Something went wrong')
     })
   }
@@ -74,6 +105,8 @@ export class TraitsAdminComponent implements OnInit {
   }
 
   update_trait() {
+    // this.socket = io.io(`http://localhost:5000`)
+    this.socket = io.io(`https://slamdunk.onrender.com`)
     const updatedTrait: Trait = {
       _id: this.id,
       name: this.title,
@@ -83,17 +116,21 @@ export class TraitsAdminComponent implements OnInit {
     }
     this.traitsService.updateTrait(updatedTrait).subscribe({
       next: () => {
+        this.socket.emit('trait', updatedTrait, updatedTrait._id, false, true)
+        $('#create_trait').modal('hide')
+
         this.toast.success(`${updatedTrait.name} successfully updated`)
-        window.location.reload()
       }, error: () => this.toast.error('Something went wrong')
     })
   }
 
   delete_trait(item: Trait) {
+    // this.socket = io.io(`http://localhost:5000`)
+    this.socket = io.io(`https://slamdunk.onrender.com`)
     this.traitsService.deleteTrait((item._id) as string).subscribe({
       next: () => {
-        this.traits = this.traits.filter(trait => trait._id !== trait._id)
-        window.location.reload()
+        this.traits = this.traits.filter(trait => trait._id !== item._id)
+        this.socket.emit('trait', this.traits, item._id, true, false)
       }, error: () => alert('Something went wrong')
     })
   }
