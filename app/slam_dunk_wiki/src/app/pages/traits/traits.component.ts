d97cd52c58fa3739f9b0e591330986bcaaddd07a
eliscioter~ <attila433lehun@gmail.com>
2023-02-11 12:58:33 +0800

    made traits real-time

--- app/slam_dunk_wiki/src/app/pages/traits/traits.component.ts
+++ app/slam_dunk_wiki/src/app/pages/traits/traits.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as io from 'socket.io-client'
+
 import { Trait } from 'src/app/interfaces/Traits';
 import { TraitsService } from 'src/app/services/traits/traits.service';
 
@@ -10,12 +12,34 @@ import { TraitsService } from 'src/app/services/traits/traits.service';
 export class TraitsComponent implements OnInit {
 
   traits: Trait[] = []
+  private socket: any
+  private tag: string = 'offense'
+
   constructor(private traitsService: TraitsService) { }
 
   ngOnInit(): void {
+    //  this.socket = io.io(`http://localhost:5000`)
+     this.socket = io.io(`https://slamdunk.onrender.com`)
     this.traitsService.getTraits().subscribe({
-      next: trait => {
-        this.traits = trait.filter(item => item.tag === 'offense')
+      next: traitData => {
+        this.socket.on('receive-traits', (trait: Trait, id: string) => {
+          if(trait.tag !== this.tag) return
+          this.traits.push({...trait, _id: id})
+        })
+        this.socket.on('receive-new-traits', (trait: Trait[]) => {
+          this.traits.splice(0, this.traits.length, ...trait.filter(item => item.tag === this.tag))
+        })
+        this.socket.on('receive-updated-traits', (trait: Trait) => {
+          const updatedTraits = this.traits.map(item => {
+            if (item._id === trait._id) {
+              return trait
+            }
+            return item
+          })
+          this.traits = updatedTraits.filter(item => item.tag === this.tag)
+          return
+        })
+        this.traits = traitData.filter(item => item.tag === this.tag)
       }
     })
   }
